28d6e11d7d34a16a3f748b06415d117a282802bb
sythoniq <188770367+sythoniq@users.noreply.github.com>
2025-12-04 07:55:52 +0300

    Finished it, quite the simple thing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Experience from './components/Experience'
 import {useState} from 'react'
 
 function App() {
+  const hyphen = "-"
   const [fullName, setFullName] = useState("");
   const [phoneNo, setPhoneNo] = useState("");
   const [emailAdd, setEmailAdd] = useState("");
@@ -51,8 +52,14 @@ function App() {
     <>
       <header className="heading">
         <h3>{fullName}</h3>
-        <p className="email-name">{emailAdd}</p>
-        <p className="phone-number">{phoneNo}</p>
+        <div className="extra-info">
+          <p className="email-name">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>email</title><path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
+            {emailAdd}</p>
+          <p className="phone-number">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>phone</title><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
+            {phoneNo}</p>
+        </div>
       </header>
       <div className="container">
         <div className="forms">
@@ -64,17 +71,24 @@ function App() {
           <div className="education-container">
             <h3 className="edu-title">Education</h3>
             <div className="education-content">
-              <div className="education-dates">
-                <h4>{edStart}</h4>
-                <h4>{edEnd}</h4>
-              </div>
               <h3>{degree}</h3>
+              <div className="education-dates">
+                <p>{edStart} {hyphen} {edEnd}</p>
+              </div>
             </div>
           </div>
           <div className="experience-container">
             <h3 className="exp-title">Experience</h3>
             <div className="experience-content">
-              <h3>{cName}</h3>
+              <div className="experience-extras">
+                <h3>{cName}</h3>
+                <div className="experience-dates"> 
+                  <p>{cStart} {hyphen} {cEnd}</p>
+                </div>
+              </div>
+              <div className="experience-description">
+                <p>{cDesc}</p>
+              </div>
             </div>
           </div>
         </div>
